d095aa0d0bb369482779fe2cd40d41cbdaea826f
thesmallstar <manthan.surkar@gmail.com>
2019-07-17 03:21:14 +0530

    Final Commit Added logout and successful Singin

--- client/app/components/Home/Home.js
+++ client/app/components/Home/Home.js
@@ -9,21 +9,37 @@ class Home extends Component {
     this.state = {
       isLoading: true,
       token: "",
+      SignInemail: "",
+      SignInpassword: "",
+
+      SignUpEmail: "",
+      SignUpPassword: "",
+      SignUpFN: "",
+      SignUpLN: "",
+
       SignupError: "",
       SigninError: ""
     };
+    this.onTextBoxChange = this.onTextBoxChange.bind(this);
+
+    this.signin = this.signin.bind(this);
+    this.signup = this.signup.bind(this);
+
+    this.logout = this.logout.bind(this);
   }
 
   componentDidMount() {
-    const token = getFromStorage("login_token");
+    const obj = getFromStorage("login_token");
 
-    if (token) {
-      fetch("/api/verify?token=" + token)
+    if (obj && obj.token) {
+      console.log(obj);
+      const { token } = obj;
+      fetch("/api/account/verify?token=" + token)
         .then(res => res.json())
         .then(json => {
           if (json.success) {
             this.setState({
-              token,
+              token: token,
               isLoading: false
             });
           } else {
@@ -38,6 +54,81 @@ class Home extends Component {
         isLoading: false
       });
     }
+  }
+
+  signup() {
+    this.setState({
+      isLoading: true
+    });
+    //grabState
+    const { SignUpEmail, SignUpPassword, SignUpFN, SignUpLN } = this.state;
+    fetch("/api/account/signup", {
+      method: "POST",
+      body: JSON.stringify({
+        email: SignUpEmail,
+        password: SignUpPassword,
+        firstname: SignUpFN,
+        lastname: SignUpLN
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    })
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          SignupError: json.messege,
+          isLoading: false
+        });
+      });
+  }
+
+  signin() {
+    console.log("here");
+    this.setState({
+      isLoading: true
+    });
+    //grabState
+    const { SignInemail, SignInpassword } = this.state;
+    fetch("/api/account/signin", {
+      method: "POST",
+      body: JSON.stringify({
+        email: SignInemail,
+        password: SignInpassword
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    })
+      .then(res => res.json())
+      .then(json => {
+        if (json.success) {
+          console.log("here");
+          setInStorage("login_token", { token: json.token });
+          this.setState({
+            token: json.token
+          });
+        }
+        this.setState({
+          SigninError: json.messege,
+          isLoading: false
+        });
+      });
+  }
+
+  logout() {
+    this.setState({
+      isLoading: true
+    });
+    const { token } = this.state;
+    fetch("/api/account/logout?token=" + token)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoading: false,
+          token: ""
+        });
+      });
   }
   //sample get req.
   // fetch('/api/counters')
@@ -59,9 +150,26 @@ class Home extends Component {
   //       counters: data
   //     });
   //   });
+  onTextBoxChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
 
   render() {
-    const { isLoading, token } = this.state;
+    const {
+      isLoading,
+      token,
+      SigninError,
+      SignupError,
+      SignInemail,
+      SignInpassword,
+      SignUpEmail,
+      SignUpPassword,
+      SignUpFN,
+      SignUpLN
+    } = this.state;
 
     if (isLoading) {
       return (
@@ -74,7 +182,72 @@ class Home extends Component {
     if (!token) {
       return (
         <div>
-          <p>Sign in</p>
+          <div>
+            <p>Sign in</p>
+            {SigninError ? <p>{SigninError}r</p> : null}
+
+            <input
+              type="text"
+              onChange={this.onTextBoxChange}
+              placeholder="email"
+              name="SignInemail"
+              value={SignInemail}
+            />
+
+            <br />
+            <input
+              type="password"
+              onChange={this.onTextBoxChange}
+              placeholder="Password"
+              name="SignInpassword"
+              value={SignInpassword}
+            />
+            <br />
+            <button onClick={this.signin}>SignIN</button>
+            <br />
+          </div>
+          <br />
+          <div>
+            <p> Sign Up</p>
+            {SignupError ? <p>{SignupError}</p> : null}
+            <input
+              type="text"
+              onChange={this.onTextBoxChange}
+              name="SignUpFN"
+              placeholder="First Name"
+              value={SignUpFN}
+            />
+            <br />
+            <input
+              type="text"
+              onChange={this.onTextBoxChange}
+              name="SignUpLN"
+              placeholder="Last Name"
+              value={SignUpLN}
+            />
+            <br />
+            <input
+              type="text"
+              onChange={this.onTextBoxChange}
+              placeholder="email"
+              name="SignUpEmail"
+              value={SignUpEmail}
+            />
+            <br />
+
+            <input
+              type="password"
+              onChange={this.onTextBoxChange}
+              placeholder="Password"
+              name="SignUpPassword"
+              value={SignUpPassword}
+            />
+            <br />
+            <button onClick={this.signup}>Sign UP</button>
+          </div>
+
+          <div />
+
           <p>Sign up</p>
         </div>
       );
@@ -83,6 +256,8 @@ class Home extends Component {
     return (
       <>
         <p>Account</p>
+        <br />
+        <button onClick={this.logout}>Logout</button>
       </>
     );
   }
